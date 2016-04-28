c98231520d541e4ff288ce03812c21f6286378ec
Joe-Wilson <ratratus50@gmail.com>
2016-04-28 09:49:48 -0400

    Cleaned up the code a bit

--- src/lib/IORead.java
+++ src/lib/IORead.java
@@ -20,61 +20,60 @@ public class IORead implements KeyListener, MouseListener, MouseMotionListener, 
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
 		events.add(new IOEvent("mouseMoved", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("mouseClicked", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("mouseEntered", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("mouseExited", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+		
 		events.add(new IOEvent("mousePressed", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("mouseReleased", e.getX(), e.getY()));
 	}
 
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent arg0) {
-		// TODO Auto-generated method stub
+
 		events.add(new IOEvent("mouseWheelMoved", 100, 100));
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("keyPressed", e.getKeyChar()));
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("keyReleased", e.getKeyChar()));
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+	
 		events.add(new IOEvent("keyTyped", e.getKeyChar()));
 	}
 
